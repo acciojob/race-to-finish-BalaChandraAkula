e9854e7e6e57c2da29e7e9b89fd83676d4e112ec
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-03-21 15:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+ promises = [
+  new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 5000 + 1000)),
+];
+
+Promise.any(promises).then((result) => {
+  document.getElementById('output').innerHTML = result;
+}).catch((error) => {
+  console.error(error);
+});
